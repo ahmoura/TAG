39e893ad13eca2dd4c2cf8fa60d13fdde563f351
A. Henrique <ahmoura@live.com>
2017-10-25 10:44:09 -0200

    leitura
    Correcao da funcao de leitura

--- p3/grafo.cpp
+++ p3/grafo.cpp
@@ -53,25 +53,30 @@ int loadfile (graph &g){
 
 
   if(myfile.is_open()){ // Se abrir o arquivo, exercuta
-    for (j = 0; j < 100; j++){
+    
+		//------------------------- LEITURA DOS PROFESSORES
+		for (j = 0; j < 100; j++){
 
 		getline(myfile, line);
 		i = 0;
 
+		//------------------------- LEITURA DO CODIGO
 		while (line[i] != '|') { // Le valores do line ate o pipe
 			aux.push_back(line[i]); // Adiciona no auxiliar para futura conversao
 			i++;
 		}
-		aux_tvertex.code = aux;
+		aux_tvertex.code = aux; 
 		aux.clear();
 
+		//------------------------- LEITURA DA LICENCA
 		i += 1; // Pula o pipe
 		while (line[i] != '|') { // Le valores do line ate o pipe
 			aux.push_back(line[i]); // Adiciona no auxiliar para futura conversao
 			i++;
 		}
-		aux_tvertex.license = atoi(aux.c_str());
-
+		aux_tvertex.license = atoi(aux.c_str()); // Joga dentro da struct o valor convertido
+		
+		//------------------------- LEITURA DAS PREFERENCIAS
 		while(i < line.size()) { // Le ate o final da linha
 			while (line[i] != ' ') { // Le os numeros entre espacos
 				aux.push_back(line[i]); // Adiciona ao vetor auxiliar
@@ -84,6 +89,31 @@ int loadfile (graph &g){
 		g.teacher.push_back(aux_tvertex); // Adiciona o vertice auxiliar no vetor do grafo
 		aux_tvertex.preferences.clear(); // Limpa dados do vetor de arestas do vertice auxiliar
     }
+	
+	//------------------------- LEITURA DAS ESCOLAS
+	for (j = 0; j < 50; j++){
+
+		getline(myfile, line);
+		i = 0;
+
+		//------------------------- LEITURA DO CODIGO
+		while (line[i] != '|') { // Le valores do line ate o pipe
+			aux.push_back(line[i]); // Adiciona no auxiliar para futura conversao
+			i++;
+		}
+		aux_svertex.code = aux;
+		aux.clear();
+
+		//------------------------- LEITURA DA PREFERENCIA
+		i += 1; // Pula o pipe
+		while (line[i] != '|') { // Le valores do line ate o pipe
+			aux.push_back(line[i]); // Adiciona no auxiliar para futura conversao
+			i++;
+		}
+		aux_tvertex.preferences = atoi(aux.c_str());
+		g.school.push_back(aux_svertex); // Adiciona o vertice auxiliar no vetor do grafo
+		
+	}
     myfile.close(); // Fecha o arquivo apos finalizacao da leitura
   }
   else cout << "Unable to open file"; // RETURN -1;
